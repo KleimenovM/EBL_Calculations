8aa787ae5bd9a14d0e5d9f2a8e4938d1767a105c
KleimenovM <kleimenov.mi@phystech.edu>
2024-10-25 01:12:05 +0200

    total cross-section calculation added

--- src/cross_section.py
+++ src/cross_section.py
@@ -1,5 +1,7 @@
 import numpy as np
 from numba import njit
+from scipy.special import spence
+
 from config.constants import SIGMA_TH, M_E2
 
 
@@ -31,6 +33,27 @@ def gamma_gamma_cross_section(e0, e, z, mu):
     b = ((b2 + np.abs(b2)) / 2.0) ** 0.5  # = b2 if b2 > 0; = 0 if b2 < 0
     multiplicator = -4 * b + 2 * b ** 3 + (3 - b ** 4) * np.log((1 + b) / (1 - b))
     return 3 / 16 * SIGMA_TH * (1 - b ** 2) * multiplicator
+
+
+def p(x):
+    ln2 = np.log(2)
+    ln_1mx = np.log(1-x)
+    ln_1px = np.log(1+x)
+    li2 = spence(1 - (1 - x) / 2)  # see definition of spence function in scipy
+    return (ln2**2 - np.pi**2 / 6 + 2 * li2 - x * (1+x**2) / (1 - x**2) + (ln_1px - 2 * ln2) * ln_1mx +
+            1/2 * (ln_1mx**2 - ln_1px**2) + (1+x**4) / (2 * (1 - x**2)) * (ln_1px - ln_1mx))
+
+
+def total_cross_section(e0, z, e):
+    """
+    Total cross-section integrated by all angles
+    :param e0: [eV], first incident photon energy
+    :param e: [eV], second incident photon energy
+    :param z: [DL], redshift
+    :return: [m2], cross-section
+    """
+    b_max = beta(e0, e, z, -1)
+    return 3/4 * SIGMA_TH * (1+z)**(-2) * (M_E2 / (e0 * e))**2 * p(b_max) * np.heaviside(b_max, 0.0)
 
 
 if __name__ == '__main__':
