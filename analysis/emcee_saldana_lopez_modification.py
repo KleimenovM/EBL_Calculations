52852789f5291e42bd63e250d6c6c0393c151fe8
KleimenovM <kleimenov.mi@phystech.edu>
2025-01-28 09:20:38 +0100

    independent emcee markov chains added for a number of sources and one alpha parameter

--- analysis/emcee_saldana_lopez_modification.py
+++ analysis/emcee_saldana_lopez_modification.py
@@ -1,12 +1,16 @@
+import os
 import matplotlib.pyplot as plt
 import numpy as np
 import emcee as emc
 import pandas as pd
+import seaborn as sns
+import multiprocessing as mp
+import pickle as pck
+
 from scipy.interpolate import CubicSpline
 
-import seaborn as sns
-
-from src.ebl_photon_density import EBLSaldanaLopez
+from config.settings import DATA_DIR
+from src.ebl_photon_density import EBLSaldanaLopez, EBL
 from src.optical_depth import OpticalDepth, OpticalDepthInterpolator
 from src.source_base import SourceBase, Source
 from src.source_spectra import GreauxModel
@@ -38,7 +42,7 @@ class FastSL:
         return
 
 
-class OneParameterSLBasedModel:
+class FastOneParameterSLBasedModel:
     def __init__(self, fast_SL: FastSL, gm: GreauxModel):
         self.fast_SL = fast_SL
         self.gm = gm
@@ -46,6 +50,21 @@ class OneParameterSLBasedModel:
     def __call__(self, e, alpha, gamma, beta, eta, lam, eps):
         mod_e = e * np.exp(-eps)
         tau = self.fast_SL(mod_e)
+        return np.exp(-alpha * tau) * self.gm.get(mod_e, gamma, beta, eta, lam)
+
+
+class OneParameterSLBasedModel:
+    def __init__(self, odi: OpticalDepthInterpolator, source: Source, gm: GreauxModel):
+        self.odi = odi
+        self.gm = gm
+        self.source = source
+
+    def __call__(self, e, alpha, gamma, beta, eta, lam, eps):
+        lg_e = np.log10(e)
+        lg_mod_e = lg_e - eps
+        mod_e = 10 ** lg_mod_e
+        z_line = np.ones_like(lg_mod_e) * self.source.z
+        tau = self.odi.interpolator((z_line, lg_mod_e))
         return np.exp(-alpha * tau) * self.gm.get(mod_e, gamma, beta, eta, lam)
 
 
@@ -103,11 +122,38 @@ def check_the_sampler(sampler, ndim):
     return
 
 
-def simple_sl_modification():
-    sb = SourceBase()
-    source: Source = sb(1)
-    # source.plot_spectrum()
+class SimpleSLModification:
+    def __init__(self, optical_depth_interpolator: OpticalDepthInterpolator,
+                 nwalkers: int = 32, nsteps: int = 1000):
+        self.odi = optical_depth_interpolator
 
+        self.ndim = 6
+        self.theta0 = np.array([1.0, 2.0, 0.0, 0.0, 0.0, 0.0])
+        self.sigmas = np.array([0.2, 3.0, 2.0, 4.0, 2.0, 0.1])
+
+        self.nwalkers = nwalkers
+        self.nsteps = nsteps
+        return
+
+    def run(self, source: Source):
+        pos = self.theta0 + self.sigmas / 2 * np.random.randn(self.nwalkers, self.ndim)
+
+        gm = GreauxModel(name=source.title,
+                         phi0=source.phi0,
+                         e0=source.e0,
+                         gamma=2.0, beta=0.0, eta=0.0, lam=0.0)
+
+        op_SL_model = OneParameterSLBasedModel(self.odi, source, gm)
+
+        sampler = emc.EnsembleSampler(self.nwalkers, self.ndim, log_probability,
+                                      args=(source, op_SL_model))
+
+        sampler.run_mcmc(pos, self.nsteps, progress=True)
+
+        return op_SL_model, sampler.get_chain(discard=200, thin=25, flat=True)
+
+
+def simple_sl_modification(source, nsteps: int = 1000):
     theta0 = np.array([1.0, 2.0, 0.0, 0.0, 0.0, 0.0])
     sigmas = np.array([0.2, 3.0, 2.0, 4.0, 2.0, 0.1])
 
@@ -119,21 +165,23 @@ def simple_sl_modification():
                      e0=source.e0,
                      gamma=2.0, beta=0.0, eta=0.0, lam=0.0)
 
-    op_SL_model = OneParameterSLBasedModel(FastSL(source), gm)
+    op_SL_model = FastOneParameterSLBasedModel(FastSL(source), gm)
 
     sampler = emc.EnsembleSampler(nwalkers, ndim, log_probability,
                                   args=(source, op_SL_model))
 
-    sampler.run_mcmc(pos, 500, progress=True)
+    sampler.run_mcmc(pos, nsteps, progress=True)
 
-    # print(sampler.get_autocorr_time())
-
+    # print(sampler.get_autocorr_time()
     # check_the_sampler(sampler, ndim)
 
     flat_samples = sampler.get_chain(discard=200, thin=25, flat=True)
-    print(flat_samples.shape)
 
-    e1 = 10**np.linspace(np.log10(source.e_ref[0]), np.log10(source.e_ref[-1]), 100)
+    return [op_SL_model, flat_samples]
+
+
+def plot_all_the_spectra(flat_samples, source, op_SL_model, if_show=False):
+    e1 = 10 ** np.linspace(np.log10(source.e_ref[0]), np.log10(source.e_ref[-1]), 100)
 
     for s in flat_samples:
         res = op_SL_model(e1, *s)
@@ -142,18 +190,55 @@ def simple_sl_modification():
         # print(res)
     plt.xscale('log')
     plt.yscale('log')
-    source.plot_spectrum()
+    source.plot_spectrum(if_show=if_show)
+    return
 
-    # columns = ["alpha", "gamma", "beta", "eta", "lam", "eps"]
-    # fs = pd.DataFrame(flat_samples, columns=columns)
-    #
-    # sns.pairplot(fs, diag_kind="kde", kind="hist", corner=True)
-    # # plt.hist(fs["alpha"], bins=30)
-    # print(fs["alpha"].mean(), fs["alpha"].std())
+
+def plot_hist(flat_samples):
+    columns = ["alpha", "gamma", "beta", "eta", "lam", "eps"]
+    fs = pd.DataFrame(flat_samples, columns=columns)
+
+    sns.pairplot(fs, diag_kind="kde", kind="hist", corner=True)
+    # plt.hist(fs["alpha"], bins=30)
+    print(fs["alpha"].mean(), fs["alpha"].std())
+    plt.show()
+    return
+
+
+def single_source_main():
+    sb = SourceBase()
+    source: Source = sb(1)
+    op_SL_model, flat_samples = simple_sl_modification(source)
+    # plot_all_the_spectra(flat_samples, source, op_SL_model)
+    plot_hist(flat_samples, source, op_SL_model)
+    return
+
+
+def several_sources_main():
+    sb = SourceBase(if_min_evt=True, min_evt=5)
+    # source_ids = np.arange(1, 100, 1)
+    sources = [sb(i) for i in range(sb.n)]
+
+    odi = OpticalDepthInterpolator(OpticalDepth(ebl=EBLSaldanaLopez()))
+    ssl_mod = SimpleSLModification(odi, nwalkers=32, nsteps=1500)
+
+    with mp.Pool(processes=mp.cpu_count()) as pool:
+        results = pool.map(ssl_mod.run, sources)
+
+    pck.dump(results, os.path.join(DATA_DIR, "results.pck"))
+
+    plt.figure(figsize=(10, 10))
+    joint_result = pd.DataFrame(results[0][1], columns=["alpha", "gamma", "beta", "eta", "lam", "eps"])
+    for i, s in enumerate(sources):
+        # plt.subplot(4, 3, i + 1)
+        # plot_all_the_spectra(results[i][1], s, results[i][0])
+        pd.concat([joint_result, pd.DataFrame(results[i][1])])
+
+    plot_hist(joint_result)
     # plt.show()
-
     return
 
 
 if __name__ == '__main__':
-    simple_sl_modification()
+    # single_source_main()
+    several_sources_main()
--- src/source_base.py
+++ src/source_base.py
@@ -23,13 +23,14 @@ class Source:
         self.e0 = np.sqrt(self.e_ref[0] * self.e_ref[-1])
         self.phi0 = np.sqrt(self.dnde[0] * self.dnde[-1])
 
-    def plot_spectrum(self):
+    def plot_spectrum(self, if_show: bool = False):
         # plt.figure(figsize=(8, 6))
-        plt.errorbar(self.e_ref, self.dnde, yerr=[self.dnde_errn, self.dnde_errp], linestyle='', marker='o')
+        plt.errorbar(self.e_ref, self.dnde, yerr=[self.dnde_errn, self.dnde_errp], linestyle='', marker='o', color='red')
         plt.plot(self.e_ref, self.phi0 * (self.e_ref / self.e0)**(-2), color='black', linestyle='--')
         plt.xscale('log')
         plt.yscale('log')
-        plt.show()
+        if if_show:
+            plt.show()
         return
 
 
